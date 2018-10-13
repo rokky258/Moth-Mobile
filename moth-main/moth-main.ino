203f2453ab054b6006c63b0b3faf4dc4d2e45594
Adam Powell <43593885+adamtpowell@users.noreply.github.com>
2018-10-13 18:57:45 -0400

    Reverted movement to previous version and tried something to fix the angle

--- moth-main/moth-main.ino
+++ moth-main/moth-main.ino
@@ -1,15 +1,5 @@
 #include <L298N.h>
 
-// Initialize Motors
-#define ENA 10
-#define IN1 9
-#define IN2 8
-#define IN3 7
-#define IN4 6
-#define ENB 5
-L298N motorA(ENA, IN1, IN2);
-L298N motorB(ENB, IN3, IN4);
-
 // Set array of sensor pins
 // A5 Front, A4 Back, A3 Left, A2 Right
 int sensorPins[] = {A5, A4, A3, A2};
@@ -23,13 +13,7 @@ int right = 0;
 // Direction movement
 double xDirection, yDirection = 0;
 
-#define base_speed 80
-
 void setup() {
-
-  // Set up motor speed
-  motorA.setSpeed(80); // an integer between 0 and 255
-  motorB.setSpeed(80);
   Serial.begin(9600);
 }
 
@@ -40,12 +24,13 @@ void loop() {
   back = 1000 - analogRead(A4);
   left = 1000 - analogRead(A3);
   right = 1000 - analogRead(A2);
-  testCalculateDirection(front, back, left, right);
-  delay(5000);
+
+ // testCalculateDirection(front, back, left, right);
+  drive_at_angle(calculateDirection(front,back,left,right),150);
+  delay(1000);
 }
 
 void testCalculateDirection(int front, int back, int left, int right) {
- /*
   Serial.print("up:");
   Serial.println(front);
   Serial.print("down:");
@@ -54,42 +39,12 @@ void testCalculateDirection(int front, int back, int left, int right) {
   Serial.println(left);
   Serial.print("right:");
   Serial.println(right);
-  */
-  drive_at_angle(calculateDirection(front, back, left, right), 60);
-  drive_at_angle(1.57,base_speed);
-  delay(2000);
-  drive_at_angle(0.85,base_speed);
-  delay(2000);
-  drive_at_angle(0,base_speed);
-  delay(2000);
-  drive_at_angle(0.85,base_speed);
-  delay(2000);
-  drive_at_angle(1.57,base_speed);
-//  delay(2000);
-  delay(2000);
+  Serial.print("direction:");
+  Serial.println(calculateDirection(front, back, left, right));
 }
 
 double calculateDirection(int up, int down, int left, int right){
   yDirection = up - down;
   xDirection = right - left;
   return(atan2(yDirection,xDirection));
-}
-
-
-void drive_at_angle(double rad_angle, double sp){
-   double mapped_val = (sp / PI) * rad_angle; 
-   Serial.print(mapped_val);
-   Serial.print(" : "); 
-   set_wheel(motorA, mapped_val);
-   Serial.println(sp - mapped_val);
-   set_wheel(motorB, sp - mapped_val);
-}
-
-int set_wheel(L298N motor, int sp) {
-  if (sp >= 0){
-    motor.forward();
-  } else {
-    motor.backward();
-  }
-  motor.setSpeed(sp);
 }
--- moth-main/moth-move.ino
+++ moth-main/moth-move.ino
@@ -1,51 +1,75 @@
-///*#include <L298N.h>
-//
-//#define ENA 10
-//#define IN1 9
-//#define IN2 8
-//#define IN3 7
-//#define IN4 6
-//#define ENB 5
-//L298N motorA(ENA, IN1, IN2);
-//L298N motorB(ENB, IN3, IN4);
-//
-//int time_delay = 500;
-//int base_speed = 70;
-//
-//void run_movement_test()
-//{
-//  motorA.setSpeed(120); // an integer between 0 and 255
-//  motorB.setSpeed(120);
-//  motorA.forward();
-//  motorB.forward();
-//  /*
-//  drive_at_angle(1.57,base_speed);
-//  delay(2000);
-//  drive_at_angle(0.85,base_speed);
-//  delay(2000);
-//  drive_at_angle(0,base_speed);
-//  delay(2000);
-//  drive_at_angle(0.85,base_speed);
-//  delay(2000);
-//  drive_at_angle(1.57,base_speed);
-//  delay(2000);
-//  motorA.stop();
-//  motorB.stop();
-//  delay(10000);
-//  
-//}
-//*/
-//void drive_at_angle(double rad_angle, double sp){
-//   double mapped_val = (sp / PI) * rad_angle; 
-//   set_wheel(motorA, mapped_val);
-//   set_wheel(motorB, sp - mapped_val);
-//}
-//
-//int set_wheel(L298N motor, int sp) {
-//  if (sp > 0){
-//    motor.forward();
-//  } else {
-//    motor.backward();
-//  }
-//  motor.setSpeed(sp);
-//}*/
+
+const int ENA = 6;
+const int IN1 = 8;
+const int IN2 = 7;
+const int IN3 = 2;
+const int IN4 = 4;
+const int ENB = 3;
+L298N driver(ENA,IN1,IN2,IN3,IN4,ENB); 
+int time_delay = 500;
+int base_speed = 300;
+int w_left = L298N::MOTOR_A;
+int w_right = L298N::MOTOR_B;
+
+void test_driving() {
+  drive_at_angle(1.57,base_speed);
+  delay(2000);
+  drive_at_angle(0.85,base_speed);
+  delay(2000);
+  drive_at_angle(0,base_speed);
+  delay(2000);
+  drive_at_angle(0.85,base_speed);
+  delay(2000);
+  drive_at_angle(1.57,base_speed);
+  delay(2000);
+}
+
+void drive_at_angle(double angle, double sp){
+//   int right_speed, left_speed;
+//   if (rad_angle < (-PI/2)) {
+//      left_speed = 0; // MIN TURN SPEED
+//   }
+//   else if (rad_angle < 0){
+//    right_speed = 0;
+//   }
+//   else{
+//   if (rad _angle > (PI/2))
+//   rad_angle /= (PI / 2);
+//   rad_angle = abs(rad_angle);
+//   while (rad_angle > PI / 2) {
+//    rad_angle -= PI / 2;
+//   }
+Serial.print("Angle: ");
+   Serial.println(angle);
+      if (angle < 0) {
+        angle = (M_PI*2) + angle;
+      }
+
+      if (angle > 2.987 && angle <= 2*PI/3){
+        angle = 2.987;
+      } else if (angle < 0.524 || angle > (3*PI)/2){
+        angle = 0.524;
+      }
+   //int real_speed = sp * (angle / (PI / 2));
+   
+  
+
+   double mapped_val = (sp / PI) * angle; 
+   Serial.print("Angle: ");
+   Serial.println(angle);
+   Serial.print("Mapped val: ");
+   Serial.println(mapped_val);
+   Serial.print("Speed - Mapped_val: ");
+   Serial.println(sp - mapped_val);
+   set_wheel(w_right, mapped_val);
+   set_wheel(w_left, sp - mapped_val);
+}
+
+int set_wheel(int motor, int speed) {
+  if (speed > 0){
+    driver.setup_motor(motor,HIGH,LOW);
+  } else {
+    driver.setup_motor(motor,LOW,HIGH);
+  }
+  driver.drive_motor(motor,speed);
+}
