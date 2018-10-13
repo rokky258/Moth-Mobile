da90d9d1e60e26c3575f06c13af76400be94cfb1
aungkhantko <aungkhantko@protonmail.com>
2018-10-13 04:48:08 -0400

    test

--- moth-main/moth-main.ino
+++ moth-main/moth-main.ino
@@ -1,30 +1,61 @@
 #include <L298N.h>
-  // put your setup code here, to run once:
-int sensorPins[] = {A5,A4,A3,A2}; // select the input pin for the potentiometer
+
+#define ENA 10
+#define IN1 9
+#define IN2 8
+#define IN3 7
+#define IN4 6
+#define ENB 5
+L298N motorA(ENA, IN1, IN2);
+L298N motorB(ENB, IN3, IN4);
+
+// put your setup code here, to run once:
+int sensorPins[] = {A5, A4, A3, A2}; // select the input pin for the potentiometer
+
 //A5 Front, A4 Back, A3 Left, A2 Right
 int ledPin = 13; // select the pin for the LED
 int sensorValues[] = {0,0,0,0}; // vasriable to store the value coming from the sensor
+
+int front = 0;
+int back = 0;
+int left = 0;
+int right = 0;
+
 double xDirection, yDirection = 0;
+
+
 void setup() {
-  pinMode(ledPin, OUTPUT);
+  // Test Values
+  front = 10;
+  motorA.setSpeed(80); // an integer between 0 and 255
+  motorB.setSpeed(80);
   Serial.begin(9600);
+  //run_movement_test();
 }
 
 
 void loop() {
-  for (int i = 0; i < 4; i ++) {
-    sensorValues[i] = 1000 - analogRead(sensorPins[i]);
+  front = 1000 - analogRead(A5);
+  back = 1000 - analogRead(A4);
+  //left = 1000 - analogRead(A3);
+  //right = 1000 - analogRead(A2);
+  if (front >= back) {
+    motorA.forward();
+    motorB.forward();  
+  } else {
+    motorA.backward();
+    motorB.backward();
   }
 }
 
 void testCalculateDirection() {
   Serial.print("up:");
   Serial.println(sensorValues[0]);
-    Serial.print("down:");
+  Serial.print("down:");
   Serial.println(sensorValues[1]);
-    Serial.print("left:");
+  Serial.print("left:");
   Serial.println(sensorValues[2]);
-    Serial.print("right:");
+  Serial.print("right:");
   Serial.println(sensorValues[3]);
   delay(2000);
   Serial.println(calculateDirection(sensorValues[0],sensorValues[1],sensorValues[2],sensorValues[3]), DEC);
--- moth-main/moth-move.ino
+++ moth-main/moth-move.ino
@@ -1,45 +1,51 @@
-
-#define ENA 10
-#define IN1A 9
-#define IN2A 8
-#define ENB 5
-#define IN1B 7
-#define IN2B 6
-
-L298N motorA(ENA, IN1A, IN2A);
-L298N motorB(ENB, IN1B, IN2B);
-
-int time_delay = 500;
-int base_speed = 70;
-
-void run_movement_test()
-{
-   drive_at_angle(1.57,base_speed);
-  delay(2000);
-  drive_at_angle(0.85,base_speed);
-  delay(2000);
-  drive_at_angle(0,base_speed);
-  delay(2000);
-  drive_at_angle(0.85,base_speed);
-  delay(2000);
-  drive_at_angle(1.57,base_speed);
-  delay(2000);
-  motorA.stop();
-  motorB.stop();
-  delay(10000);
-}
-
-void drive_at_angle(double rad_angle,double sp){
-   double mapped_val = (sp/ PI) * rad_angle; 
-   set_wheel(motorA, mapped_val);
-   set_wheel(motorB, sp - mapped_val);
-}
-
-int set_wheel(L298N motor, int sp) {
-  if (sp > 0){
-    motor.forward();
-  } else {
-    motor.backward();
-  }
-  motor.setSpeed(sp);
-}
+///*#include <L298N.h>
+//
+//#define ENA 10
+//#define IN1 9
+//#define IN2 8
+//#define IN3 7
+//#define IN4 6
+//#define ENB 5
+//L298N motorA(ENA, IN1, IN2);
+//L298N motorB(ENB, IN3, IN4);
+//
+//int time_delay = 500;
+//int base_speed = 70;
+//
+//void run_movement_test()
+//{
+//  motorA.setSpeed(120); // an integer between 0 and 255
+//  motorB.setSpeed(120);
+//  motorA.forward();
+//  motorB.forward();
+//  /*
+//  drive_at_angle(1.57,base_speed);
+//  delay(2000);
+//  drive_at_angle(0.85,base_speed);
+//  delay(2000);
+//  drive_at_angle(0,base_speed);
+//  delay(2000);
+//  drive_at_angle(0.85,base_speed);
+//  delay(2000);
+//  drive_at_angle(1.57,base_speed);
+//  delay(2000);
+//  motorA.stop();
+//  motorB.stop();
+//  delay(10000);
+//  
+//}
+//*/
+//void drive_at_angle(double rad_angle, double sp){
+//   double mapped_val = (sp / PI) * rad_angle; 
+//   set_wheel(motorA, mapped_val);
+//   set_wheel(motorB, sp - mapped_val);
+//}
+//
+//int set_wheel(L298N motor, int sp) {
+//  if (sp > 0){
+//    motor.forward();
+//  } else {
+//    motor.backward();
+//  }
+//  motor.setSpeed(sp);
+//}*/
